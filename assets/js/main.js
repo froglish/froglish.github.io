b4fc523db2fcc07e677ba512c7db93d3985174e8
Stan Chollet <stanislas.chollet@gmail.com>
2016-04-11 08:01:18 +0200

    :relaxed: function 'logEvent' can only used in Facebook Canvas environment

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
   $('.article-like').on('click', function() {
     // Push event to Google analytics
     ga('send', 'event', $(this).attr('data-product'), 'click', 'Landing v1');
-    FB.AppEvents.logEvent('RATED', 5, {
-      'CONTENT_ID': $(this).attr('data-product'),
-      'MAX_RATING_VALUE': 5
-    });
     $(this).toggleClass('liked');
   });
 });
