a08af3a2daf9af67872eb9ea6c4859c7f8ded03f
Stan Chollet <stanislas.chollet@gmail.com>
2016-04-11 07:52:02 +0200

    :statue_of_liberty: add RATED event on like

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $('.article-like').on('click', function() {
     // Push event to Google analytics
     ga('send', 'event', $(this).attr('data-product'), 'click', 'Landing v1');
+    FB.AppEvents.LogEvent('RATED', 5, {
+      'CONTENT_ID': $(this).attr('data-product'),
+      'MAX_RATING_VALUE': 5
+    });
     $(this).toggleClass('liked');
   });
 });
